8d6cbdd7441a33068f263dce6c87ee4594a3eb3b
Joshkleinv <Joshkv99@gmail.com>
2019-05-09 15:31:59 +0200

    Instructor_Activity

---
+++ src/com/gymonline/jpa/models/Instructor_Activity.java
@@ -0,0 +1,42 @@
+package com.gymonline.jpa.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@Entity
+@XmlRootElement
+@XmlType(name="Instructor_Activity", propOrder = {"iASocialSecurityNumber", "iAActivityId"})
+@JsonPropertyOrder({"iASocialSecurityNumber", "iAActivityId"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "iASocialSecurityNumber")
+public class Instructor_Activity {
+
+    @Id
+    @Column(name = "IA_Social_Security_Number")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer iASocialSecurityNumber;
+
+
+    @Column(name = "IA_Activity_Id")
+    private Integer iAActivityId;
+
+    public Integer getiASocialSecurityNumber() {
+        return iASocialSecurityNumber;
+    }
+
+    public void setiASocialSecurityNumber(Integer iASocialSecurityNumber) {
+        this.iASocialSecurityNumber = iASocialSecurityNumber;
+    }
+
+    public Integer getiAActivityId() {
+        return iAActivityId;
+    }
+
+    public void setiAActivityId(Integer iAActivityId) {
+        this.iAActivityId = iAActivityId;
+    }
+}
