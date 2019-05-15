35441bfb233c6194c4f7ebfa1ad6e02afed26d75
[pnilssson] <[p.nilsson95@hotmail.com]>
2019-05-15 15:21:38 +0200

    Added Keys Classes

--- src/com/gymonline/jpa/repositories/GymRepository.java
+++ src/com/gymonline/jpa/repositories/GymRepository.java
@@ -5,11 +5,10 @@ import com.gymonline.jpa.models.Gym;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.transaction.Transactional;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Transactional
+
 public class GymRepository {
     @PersistenceContext(unitName = "GymOnlinePU")
     private EntityManager em;
@@ -32,7 +31,7 @@ public class GymRepository {
     }
 
     public List<Gym> getSpecificActivities(int id, int activityId){
-        Query query = em.createQuery("SELECT g.gymName, g.gymRating, g.gymAdress, g.gymZipCode, g.city, a.activityId, a.activityRating, a.activityPrice, a.activityPopularity, a.activityDate, a.activityDuration, at.activityTypeName FROM Activity a JOIN a.activityType at JOIN a.activityGym WHERE g.gymId = :gymId AND a.activityId = :activityId")
+        Query query = em.createQuery("SELECT g.gymName, g.gymRating, g.gymAdress, g.gymZipCode, g.city, a.activityId, a.activityRating, a.activityPrice, a.activityPopularity, a.activityDate, a.activityDuration, at.activityTypeName FROM Activity a JOIN a.activityType at JOIN a.activityGym g WHERE g.gymId = :gymId AND a.activityId = :activityId")
                 .setParameter("gymId", id)
                 .setParameter("activityId", activityId);
         return query.getResultList();
