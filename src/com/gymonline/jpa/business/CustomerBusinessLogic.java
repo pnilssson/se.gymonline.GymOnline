8e5f3ac30063d0564b37975fffa4033a87b48149
Joshua Klein Velderman <99joskle@mk.se>
2019-06-04 13:06:37 +0200

    businesscustomer

--- src/com/gymonline/jpa/business/CustomerBusinessLogic.java
+++ src/com/gymonline/jpa/business/CustomerBusinessLogic.java
@@ -1,11 +1,34 @@
 package com.gymonline.jpa.business;
 
+import com.gymonline.jpa.models.Customer;
 import com.gymonline.jpa.repositories.CustomerRepository;
 
 import javax.inject.Inject;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 public class CustomerBusinessLogic {
 
     @Inject
     private CustomerRepository cur;
+
+    public Response post(Customer c) {
+        return cur.create(c);
+    }
+    public List<Customer> readAll(){
+        return cur.getAll();
+    }
+    public List<Customer> readById(int id){
+        return cur.getById(id);
+    }
+    public List<Customer> readAllByCriteria(int id){
+        return cur.getActivities(id);
+    }
+    public Response update(Customer customer){
+        return cur.update(customer);
+    }
+    public Response delete(int id){
+        return cur.delete(id);
+    }
 }
